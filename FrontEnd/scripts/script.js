da318cdfae3696a58b131fac629d5df6f0de52c7
James Defosse <jam.defosse@gmail.com>
2024-12-02 19:35:38 +0100

    add of categories tabs

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -19,6 +19,23 @@ async function getWorks() {
   try {
     const response = await fetch('http://localhost:5678/api/works');
     const works = await response.json();
+    // Récupérer les catégories
+    const categoriesNames = works.map((work) => work.category.name);
+    const projectsCategories = new Set(categoriesNames);
+    // Créer le html
+    const categoriesTabsWrapper = document.querySelector('.tabs');
+    const categoriesTabs = document.createElement('ul');
+    categoriesTabsWrapper.appendChild(categoriesTabs);
+    // Ajouter les catégories aux tabs
+    projectsCategories.forEach((element) => {
+      const categoriesTab = document.createElement('li');
+      categoriesTab.className = 'categories-tabs';
+      const categoriesTabLink = document.createElement('a');
+      categoriesTabLink.innerText = element;
+      categoriesTab.appendChild(categoriesTabLink);
+      categoriesTabs.appendChild(categoriesTab);
+    });
+    //
     createWorks(works);
   } catch (error) {
     console.error(`Une erreur est survenue : ${error}`);
